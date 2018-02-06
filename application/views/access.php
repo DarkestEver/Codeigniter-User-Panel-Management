eaf533ae18ddf18a6987ea5033a797ce0954e0d1
pars11 <sametay153@gmail.com>
2018-02-06 11:45:49 +0300

    First commit with turkish translation

---
+++ application/views/access.php
@@ -0,0 +1,15 @@
+<div class="content-wrapper">    
+    <section class="content-header">
+      <h1>
+        Erşim Reddedildi
+        <small>Bu sayfaya giriş yetkiniz yok.</small>
+      </h1>
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <img src="<?php echo base_url() ?>assets/images/access.png" alt="Access Denied Image" />
+            </div>
+        </div>
+    </section>
+</div>
---
+++ application/views/changePassword.php
@@ -0,0 +1,96 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Şifre Değiştir
+        <small>Yeni bir şifre belirleyin</small>
+      </h1>
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-md-4">
+              <!-- general form elements -->
+                <div class="box box-primary">
+                    <div class="box-header">
+                        <h3 class="box-title">Bilgilerinizi Giriniz</h3>
+                    </div><!-- /.box-header -->
+                    <!-- form start -->
+                    <form role="form" action="<?php echo base_url() ?>changePassword" method="post">
+                        <div class="box-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="inputPassword1">Eski Şifreniz</label>
+                                        <input type="password" class="form-control" id="inputOldPassword" placeholder="Eski Şifre" name="oldPassword" maxlength="20" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="inputPassword1">Yeni Şifreniz</label>
+                                        <input type="password" class="form-control" id="inputPassword1" placeholder="Yeni Şifre" name="newPassword" maxlength="20" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="inputPassword2">Yeni şifrenizi doğrulayın</label>
+                                        <input type="password" class="form-control" id="inputPassword2" placeholder="Yeni şifrenizi doğrulayın" name="cNewPassword" maxlength="20" required>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- /.box-body -->
+    
+                        <div class="box-footer">
+                            <input type="submit" class="btn btn-primary" value="Gönder" />
+                            <input type="reset" class="btn btn-default" value="Sıfırla" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <?php
+                    $this->load->helper('form');
+                    $error = $this->session->flashdata('error');
+                    if($error)
+                    {
+                ?>
+                <div class="alert alert-danger alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <?php echo $this->session->flashdata('error'); ?>                    
+                </div>
+                <?php } ?>
+                <?php  
+                    $success = $this->session->flashdata('success');
+                    if($success)
+                    {
+                ?>
+                <div class="alert alert-success alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <?php echo $this->session->flashdata('success'); ?>
+                </div>
+                <?php } ?>
+                
+                <?php  
+                    $noMatch = $this->session->flashdata('nomatch');
+                    if($noMatch)
+                    {
+                ?>
+                <div class="alert alert-warning alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <?php echo $this->session->flashdata('nomatch'); ?>
+                </div>
+                <?php } ?>
+                
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php echo validation_errors('<div class="alert alert-danger alert-dismissable">', ' <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
